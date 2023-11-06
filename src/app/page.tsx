1cef0b9406193b65481c9e8f89fdda7b4fa2e6cd
Md. Mahadi Hossain <mahadihossain28@gmail.com>
2023-11-06 23:26:59 +0600

    Final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import GalleryComponent from '@/components/GalleryComponent/GalleryComponent'
 
 export default function Home() {
   return (
     <div className='container'>
-      <h1>Hello</h1>
+      <GalleryComponent/>
     </div>
   )
 }
---
+++ src/components/GalleryComponent/GalleryComponent.tsx
@@ -0,0 +1,208 @@
+"use client"
+import React, { useRef, useState } from 'react';
+import { data } from '@/data';
+import Image from 'next/image';
+import { FaRegImage } from 'react-icons/fa';
+
+export default function GalleryComponent() {
+  const [galleryImgs, setGalleryImgs] = useState(data);
+  const [isHover, setIsHover] = useState(-1);
+  const [checked, setChecked] = useState([]);
+  const [isDragging, setIsDragging] = useState();
+  const containeRef = useRef();
+
+  function handleChange(e) {
+    let isChecked = e.target.checked;
+    let value = parseInt(e.target.value);
+    if (isChecked) {
+      setChecked([...checked, value]);
+    } else {
+      setChecked(checked.filter((id) => id !== value));
+    }
+    if (value === 0) {
+      setChecked([]);
+    }
+  }
+
+  function deleteImage() {
+    let finalImages = galleryImgs.filter((v) => {
+      return !checked.includes(v.id);
+    });
+    setGalleryImgs(finalImages);
+    setChecked([]);
+  }
+
+  function detectLeftButton(e) {
+    e = e || window.event;
+    if ('buttons' in e) {
+      return e.buttons === 1;
+    }
+    let button = e.which || e.button;
+    return button === 1;
+  }
+
+  function checkPosition(e) {
+    // Implement this function if needed.
+  }
+
+  function dragStart(e, index) {
+    if (!detectLeftButton(e)) return;
+
+    setIsDragging(index);
+
+    const container = containeRef.current;
+    const items = [...container.childNodes];
+    const dragItem = items[index];
+    let dragItemImage = dragItem.childNodes[1];
+    const firstItem = items[0];
+    const secondItem = items[1];
+    const dragData = galleryImgs[index];
+    let newData = [...galleryImgs];
+
+    const dragItemBoundigRect = dragItem.getBoundingClientRect();
+    const firstItemBoundigRect = firstItem.getBoundingClientRect();
+    const secondItemBoundigRect = secondItem.getBoundingClientRect();
+
+    dragItemImage.style.position = 'fixed';
+    dragItemImage.style.zIndex = 6000;
+    dragItemImage.style.width = dragItemBoundigRect.width + 'px';
+    dragItemImage.style.height = dragItemBoundigRect.height + 'px';
+    dragItemImage.style.top = dragItemBoundigRect.top + 'px';
+    dragItemImage.style.left = dragItemBoundigRect.left + 'px';
+    dragItemImage.style.cursor = 'grabbing';
+
+    let x = e.clientX;
+    let y = e.clientY;
+
+    document.onpointermove = dragMove;
+    
+
+
+    function dragMove(e) {
+      const posX = e.clientX - x;
+      const posY = e.clientY - y;
+
+      dragItemImage.style.transform = `translate(${posX}px, ${posY}px)`;
+
+      let targetId = e.target.id;
+      if (targetId) {
+        if (targetId > 0) {
+          dragItemImage.style.width = secondItemBoundigRect.width + 'px';
+          dragItemImage.style.height = secondItemBoundigRect.height + 'px';
+        }
+        if (targetId == 0) {
+          dragItemImage.style.width = firstItemBoundigRect.width + 'px';
+          dragItemImage.style.height = firstItemBoundigRect.height + 'px';
+        }
+
+        index = Number((index = +targetId));
+        newData = galleryImgs.filter((item) => item.id !== dragData.id);
+        newData.splice(index, 0, dragData);
+      }
+    }
+
+    document.onpointerup = dragEnd;
+
+    function dragEnd() {
+      document.onpointerup = null;
+      setIsDragging(undefined);
+      dragItemImage.style = '';
+      document.onpointermove = null;
+
+      setGalleryImgs(newData);
+    }
+  }
+
+  function handleCheckboxMouseDown(e) {
+    // Prevent event propagation to the parent onMouseDown handler
+    e.stopPropagation();
+  }
+
+  return (
+    <div className="mt-3">
+      <div className="card">
+        <div className="card-header py-3">
+          {checked.length !== 0 ? (
+            <div className="d-flex justify-content-between align-items-center mx-4">
+              <div className="d-flex align-items-center">
+                <input
+                  className="checkBoxAll me-2"
+                  id="checkbox_id"
+                  value="0"
+                  type="checkbox"
+                  checked
+                  onChange={handleChange}
+                  onMouseDown={handleCheckboxMouseDown}
+                />
+                <label className="checkBoxAllLabel" htmlFor="checkbox_id">
+                  {checked.length} {checked.length === 1 ? 'File Selected' : 'Files Selected'}
+                </label>
+              </div>
+              {checked.length === 1 ? (
+                <span className="deleteFileText" onClick={deleteImage}>
+                  Delete File
+                </span>
+              ) : (
+                <span className="deleteFileText" onClick={deleteImage}>
+                  Delete Files
+                </span>
+              )}
+            </div>
+          ) : (
+            <h5 className="mb-0">Gallery</h5>
+          )}
+        </div>
+        <div className="card-body">
+          <div className="customGrid" ref={containeRef}>
+            {galleryImgs.map(({ id, imgUrl }, index) => (
+              <div
+                key={id}
+                className={`customGridItem rounded ${isDragging === index ? 'dragging' : ''}`}
+                onMouseDown={(e) => dragStart(e, index)}
+                onMouseEnter={() => setIsHover(index)}
+                onMouseLeave={() => setIsHover(-1)}
+              >
+                <div className="overlay" id={index}></div>
+                <div className={`imageContainer ${isDragging === index ? 'dragging' : ''}`}>
+                  <Image
+                    key={index}
+                    id={index}
+
+                    src={imgUrl}
+                    width={400}
+                    height={400}
+                    alt="dfgsg"
+                    className="rounded"
+                    priority={true}
+                    style={{
+                      pointerEvents: isDragging === index ? 'none' : 'auto',
+                    }}
+                  />
+                </div>
+                {isHover === index || checked.includes(id) ? (
+                  <input
+                    className="checkBoxImage"
+                    id={`checkbox_id${index}`}
+                    name="image"
+                    value={id}
+                    checked={checked.includes(id)}
+                    type="checkbox"
+                    onChange={handleChange}
+                    onMouseDown={handleCheckboxMouseDown}
+                  />
+                ) : (
+                  ''
+                )}
+              </div>
+            ))}
+            <div className="addImageSection">
+              <FaRegImage className="addImageIcon" />
+              <p className="mt-3 mb-0 addImageText">Add Images</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
