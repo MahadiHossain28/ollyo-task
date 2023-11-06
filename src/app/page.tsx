8f50d445d227f3a5aa4ea90b60c456c3f9551b1a
Md. Mahadi Hossain <mahadihossain28@gmail.com>
2023-11-06 14:26:37 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default function Home() {
+  return (
+    <div className='container'>
+      <h1>Hello</h1>
+    </div>
+  )
+}
---
+++ src/data.tsx
@@ -0,0 +1,46 @@
+export const data = [
+    {
+        id: 1,
+        imgUrl: "/img/image-11.jpeg"
+      },
+      {
+        id: 2,
+        imgUrl: "/img/image-3.webp"
+      },
+      {
+        id: 3,
+        imgUrl: "/img/image-2.webp"
+      },
+      {
+        id: 4,
+        imgUrl: "/img/image-7.webp"
+      },
+      {
+        id: 5,
+        imgUrl: "/img/image-5.webp"
+      },
+      {
+        id: 6,
+        imgUrl: "/img/image-9.webp"
+      },
+      {
+        id: 7,
+        imgUrl: "/img/image-10.jpeg"
+      },
+      {
+        id: 8,
+        imgUrl: "/img/image-4.webp"
+      },
+      {
+        id: 9,
+        imgUrl: "/img/image-8.webp"
+      },
+      {
+        id: 10,
+        imgUrl: "/img/image-1.webp"
+      },
+      {
+        id: 11,
+        imgUrl: "/img/image-3.webp"
+      }
+  ]
